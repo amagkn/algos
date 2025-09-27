7241da70bbe74f2d40327d221f8a287199a9b843
Dmitrii Repnikov <dmitrii.amagkn@gmail.com>
2025-09-27 21:33:31 +0300

    feat: merge two sorted lists

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	head := linkedlist.Create([]int{1, 2, 3, 4})
+	l1 := linkedlist.Create([]int{1, 3, 3, 5})
+	l2 := linkedlist.Create([]int{2, 4, 6, 6, 7, 8, 10})
 
-	fmt.Println(linkedlist.ToSlice(linkedlist.Reorder(head)))
+	fmt.Println(linkedlist.ToSlice(linkedlist.MergeTwoSortedLists(l1, l2))) // [1 2 3 3 4 5 6 6 7 8 10]
 }
